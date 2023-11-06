d0e43e2c34472398797e43738fcd6d228f72b3a0
leeseohyun <applemint2003@gmail.com>
2023-11-07 01:24:51 +0900

    231107 Spring Project 생성

---
+++ lsh-demo/src/main/java/me/lsh/restapidemo/LshDemoApplication.java
@@ -0,0 +1,13 @@
+package me.lsh.restapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LshDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LshDemoApplication.class, args);
+	}
+
+}
---
+++ lsh-demo/src/test/java/me/lsh/restapidemo/LshDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lsh.restapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LshDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
