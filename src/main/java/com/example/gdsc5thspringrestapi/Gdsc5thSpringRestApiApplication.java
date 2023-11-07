534d5142cdbf0fe90f4c301d4c8f23a52e9b1dd2
yunji118 <yunji118@naver.com>
2023-11-07 23:47:05 +0900

    project created

---
+++ src/main/java/com/example/gdsc5thspringrestapi/Gdsc5thSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdsc5thspringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdsc5thSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdsc5thSpringRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gdsc5thspringrestapi/Gdsc5thSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdsc5thspringrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdsc5thSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
