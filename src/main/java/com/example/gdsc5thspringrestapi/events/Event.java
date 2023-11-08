63b995873ce326d027a54f69060cf72e3ee6f91a
yunji118 <yunji118@naver.com>
2023-11-08 17:27:11 +0900

    [하윤지] 1주차 실습 - complete

--- src/main/java/com/example/gdsc5thspringrestapi/events/Event.java
+++ src/main/java/com/example/gdsc5thspringrestapi/events/Event.java
@@ -20,8 +20,8 @@ public class Event {
     private LocalDateTime closeEnrollmentDateTime;
     private LocalDateTime beginEventDateTime;
     private LocalDateTime endEventDateTime;
-    private String location; // (optional) 이게 없으면 온라인 모임 private int basePrice; // (optional)
-    private int maxPrice; // (optional)
+    private String location;
+    private int maxPrice;
     private int limitOfEnrollment;
     private EventStatus eventStatus;
 }
