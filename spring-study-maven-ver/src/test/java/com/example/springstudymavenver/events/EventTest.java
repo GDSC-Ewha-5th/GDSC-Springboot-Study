f24584c5e56d84465f5050cc15c68d44dbc9be3d
kang <kang6@ewhain.net>
2023-11-08 16:58:30 +0900

    [ADD] Event domain

---
+++ spring-study-maven-ver/src/test/java/com/example/springstudymavenver/events/EventTest.java
@@ -0,0 +1,33 @@
+package com.example.springstudymavenver.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    public void builder(){
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API developement with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBeen(){
+        Event event = new Event();
+        String name = "Event";
+        String description = "Spring";
+
+        event.setName(name);
+        event.setDescription(description);
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+
+
+}
