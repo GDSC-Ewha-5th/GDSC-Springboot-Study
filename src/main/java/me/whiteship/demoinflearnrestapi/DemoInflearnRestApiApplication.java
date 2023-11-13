3bf5f066334b770dda3cc56bfb4344f873c3e13a
official-Cherry <reebon7@gmail.com>
2023-11-13 20:43:52 +0900

    project creation

--- src/main/java/me/whiteship/demoinflearnrestapi/DemoInflearnRestApiApplication.java
+++ src/main/java/me/whiteship/demoinflearnrestapi/DemoInflearnRestApiApplication.java
@@ -2,12 +2,11 @@ package me.whiteship.demoinflearnrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class DemoInflearnRestApiApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(DemoInflearnRestApiApplication.class, args);
     }
 
