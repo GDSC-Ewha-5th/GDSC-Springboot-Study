69e4fced4c9f11684198793085bc00eb5a18db88
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2023-11-15 09:49:47 +0900

    commit

--- src/main/java/medongseon/inflearnrestapi/events/EventController.java
+++ src/main/java/medongseon/inflearnrestapi/events/EventController.java
@@ -1,5 +1,6 @@
 package medongseon.inflearnrestapi.events;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -15,11 +16,15 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)//HAL_JSON_UTF8_VALUE를 못찾아서 이걸로 했습니다..
 public class EventController {
     private final EventRepository eventRepository;
-    public EventController(EventRepository eventRepository){
+    private final ModelMapper modelMapper;
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper){
         this.eventRepository = eventRepository;
+        this.modelMapper = modelMapper;
     }
     @PostMapping
-    public ResponseEntity createEvent(@RequestBody Event event){
+    public ResponseEntity createEvent(@RequestBody EventDto eventDto){
+        Event event = modelMapper.map(eventDto, Event.class);
+
         Event newEvent = this.eventRepository.save(event);
         URI createdUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
 
