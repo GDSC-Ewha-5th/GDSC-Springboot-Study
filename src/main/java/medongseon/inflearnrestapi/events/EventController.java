6d326ec93f3f485755583dc28ff7a59ef933622d
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2023-11-15 12:38:29 +0900

    validator

--- src/main/java/medongseon/inflearnrestapi/events/EventController.java
+++ src/main/java/medongseon/inflearnrestapi/events/EventController.java
@@ -1,10 +1,10 @@
 package medongseon.inflearnrestapi.events;
 
 import org.modelmapper.ModelMapper;
-import org.modelmapper.internal.Errors;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +19,21 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 public class EventController {
     private final EventRepository eventRepository;
     private final ModelMapper modelMapper;
-    public EventController(EventRepository eventRepository, ModelMapper modelMapper){
+
+    private final EventValidator eventValidator;
+
+    public EventController(EventRepository eventRepository, ModelMapper modelMapper, EventValidator eventValidator){
         this.eventRepository = eventRepository;
         this.modelMapper = modelMapper;
+        this.eventValidator = eventValidator;
     }
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid EventDto eventDto, Errors errors){
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+
+        eventValidator.validate(eventDto, errors);
         if(errors.hasErrors()){
             return ResponseEntity.badRequest().build();
         }
