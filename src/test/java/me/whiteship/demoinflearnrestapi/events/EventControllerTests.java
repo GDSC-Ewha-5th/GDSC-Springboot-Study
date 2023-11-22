0bae9dac18d3c7030b9584035a7468a46d6bb812
official-Cherry <reebon7@gmail.com>
2023-11-22 14:11:12 +0900

    polishing

--- src/test/java/me/whiteship/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/whiteship/demoinflearnrestapi/events/EventControllerTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,8 +16,7 @@ import java.time.LocalDateTime;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 // 단위 테스트라고 보기는 어려운 class
 @RunWith(SpringRunner.class)
@@ -53,7 +53,10 @@ public class EventControllerTests {
                         .accept(MediaTypes.HAL_JSON)
                         .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
+                // console에 보이는 내용 다 andExpect 를 통해서 검사 할 수 있음
                 .andExpect(status().isCreated())
-                .andExpect((jsonPath("id").exists()));
+                .andExpect((jsonPath("id").exists()))
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE,MediaTypes.HAL_JSON_VALUE));
     }
 }
