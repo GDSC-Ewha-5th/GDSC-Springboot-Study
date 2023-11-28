e32e08aac89b8913f26701221b71e480e9477032
official-Cherry <reebon7@gmail.com>
2023-11-28 19:11:10 +0900

    fail on unknow properties (with some errors)

--- src/test/java/me/whiteship/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/me/whiteship/demoinflearnrestapi/events/EventControllerTests.java
@@ -25,7 +25,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc // MockMvc 를 사용하기 위해서
-@EnableWebMvc
 public class EventControllerTests {
     @Autowired
     MockMvc mockMvc; // slice - 요청, 응답 검증 가능 (웹서버를 띄우지 않아서 빠름)
@@ -34,6 +33,40 @@ public class EventControllerTests {
 
     @Test
     public void createEvent() throws Exception {
+        // event Dto 사용하면 값들이 잘 들어오고
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2023,11,22,12,55))
+                .closeEnrollmentDateTime(LocalDateTime.of(2023,11,23,12,55))
+                .beginEnrollmentDateTime(LocalDateTime.of(2023,11,24,12,55))
+                .closeEnrollmentDateTime(LocalDateTime.of(2023,11,25,12,55))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("Ewha Womans University")
+                .build();
+
+        // mocking ??
+        // save 에 전달하게 되는 객체는 새로 만든 객체임
+        // -> 따라서 적용이 안되고 null 이 return 됨
+        mockMvc.perform(post("/api/events/")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaTypes.HAL_JSON)
+                        .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                // console에 보이는 내용 다 andExpect 를 통해서 검사 할 수 있음
+                .andExpect(status().isCreated())
+                .andExpect((jsonPath("id").exists()))
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE,MediaTypes.HAL_JSON_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+        ;
+    }
+
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         // event Dto 사용하면 값들이 잘 들어오고
         Event event = Event.builder()
                 .id(100)
@@ -59,14 +92,18 @@ public class EventControllerTests {
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
                         .accept(MediaTypes.HAL_JSON)
                         .content(objectMapper.writeValueAsString(event)))
-                        .andDo(print())
-                        // console에 보이는 내용 다 andExpect 를 통해서 검사 할 수 있음
-                        .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists())
+                .andDo(print())
+                // console에 보이는 내용 다 andExpect 를 통해서 검사 할 수 있음
+                .andExpect(status().isBadRequest())
+                /*
+                .andExpect((jsonPath("id").exists()))
                 .andExpect(header().exists(HttpHeaders.LOCATION))
                 .andExpect(header().string(HttpHeaders.CONTENT_TYPE,MediaTypes.HAL_JSON_VALUE))
                 .andExpect(jsonPath("id").value(Matchers.not(100)))
                 .andExpect(jsonPath("free").value(Matchers.not(true)))
+                 */
+
         ;
     }
+
 }
